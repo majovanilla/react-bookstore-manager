5449ff9a125c87c40df00c2751521ffb08752697
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2020-03-30 13:12:21 -0300

    Add initialState, reducer and store to index.js file.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './components/App';
+
+import { createStore } from 'redux';
+
+import bookReducerFactory from './reducers/books';
+
+const initialState = {
+  '1': {
+    id: '1',
+    title: 'Learning Physics',
+    category: 'Learning',
+  },
+  '2': {
+    id: '2',
+    title: 'Believers',
+    category: 'Action',
+  },
+  '3': {
+    id: '3',
+    title: 'Moby Dick',
+    category: 'Sci-Fi',
+  },
+};
+
+const bookReducer = bookReducerFactory(initialState);
+const bookStore = createStore(bookReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={bookStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );

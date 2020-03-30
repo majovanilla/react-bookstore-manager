34efd8d47ef7a18cd80fc80865aa7cf21d2d29b6
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2020-03-30 14:55:07 -0300

    Add state and handleChange to BooksForm.

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,46 +1,72 @@
 import React from 'react';
 
-const generateID = () => Math.trunc(Math.random() * 1000000000);
+const generateID = () => Math.trunc(Math.random() * 10 ** 9);
 
-const BooksForm = () => {
-  const categories = [
-    'Action',
-    'Biography',
-    'History',
-    'Horror',
-    'Kids',
-    'Learning',
-    'Sci-Fi',
-  ];
+const categories = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
 
-  return (
-    <form>
-      <h2>Book Form</h2>
-      <input type="hidden" name="id" value={generateID()} />
-      <label htmlFor="title">
-        Title:
-        {' '}
-        <input type="text" name="title" value="" />
-      </label>
-      <label htmlFor="category">
-        Category:
-        <select name="category">
-          {
-            categories.map((category, index) => {
-              const categoryKey = `CATEGORY_${index}`;
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
 
-              return (
-                <option key={categoryKey} name={category}>
-                  {category}
-                </option>
-              );
-            })
-          }
-        </select>
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+    this.handleChange = event => {
+      const { value } = event.target;
+
+      if (event.target.getAttribute('name') === 'title') {
+        this.setState({ title: value });
+      }
+
+      if (event.target.getAttribute('name') === 'category') {
+        this.setState({ category: value });
+      }
+    };
+  }
+
+  render() {
+    return (
+      <form>
+        <h2>Book Form</h2>
+        <input type="hidden" name="id" value={generateID()} />
+        <label htmlFor="title">
+          Title:
+          {' '}
+          <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+        </label>
+        <label htmlFor="category">
+          Category
+          {' '}
+          { this.state.category }
+          :
+          <select name="category" onChange={this.handleChange}>
+            {
+              categories.map((category, index) => {
+                const categoryKey = `CATEGORY_${index}`;
+
+                return (
+                  <option key={categoryKey} value={category}>
+                    {category}
+                  </option>
+                );
+              })
+            }
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
 
 export default BooksForm;
